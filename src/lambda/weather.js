1dd8153937c0ada6b5bdb8fd6b1111eeac787333
David Boyd <david.boyd@mudbath.com.au>
2018-08-08 14:52:30 +1000

    fixing lambda

--- src/lambda/weather.js
+++ src/lambda/weather.js
@@ -1,15 +1,11 @@
- const fetch = require('node-fetch');
-exports.handler = function handler(event, context, callback) {
-    const endpoint = 'http://api.openweathermap.org/data/2.5/weather?q=newcastle,au&units=metric&APPID=3cda5c2cff38ce5fcc6ae26fa96c69f5';
-      
-    fetch(endpoint)
-      .then( res => res.json() )
-      .then( body =>  {
-        const response = {
-          statusCode: 200,
-          body: JSON.stringify({ temperature: body.main.temp })
-        };
-  
-        callback(null, response);
-      });
-  }
+import fetch from "node-fetch";
+const API_ENDPOINT = 'http://api.openweathermap.org/data/2.5/weather?q=newcastle,au&units=metric&APPID=3cda5c2cff38ce5fcc6ae26fa96c69f5';
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT)
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: JSON.stringify({ temperature: data.main.temp })
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};  
